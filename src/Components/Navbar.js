1a898dce36922b9f88504bab61201b858a32aa2c
Saket Mundra <saketmundra2707@gmail.com>
2022-08-10 15:44:06 +0530

    test

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,18 +1,38 @@
-import React from "react";
+import React,{useState} from "react";
 import { AiOutlineSearch } from "react-icons/ai";
 import { Avatar, Button } from "@mui/material";
 import "../Assets/Styles/navbar.css";
 import { useDispatch, useSelector } from "react-redux";
 import { selectUserData } from "../redux/slices/userSlice";
 import { Link } from "react-router-dom";
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+
+
 
 const Navbar = ({ extraNavId }) => {
   const user = useSelector(selectUserData);
   const dispatch = useDispatch();
   const logout = () => {
+    handleClose();
     localStorage.clear();
     window.location = "/signin";
   };
+
+  const [anchorEl, setAnchorEl] = useState(null);
+  const handleClick = event => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+  const acntset=()=>{
+    handleClose();
+    // window.location = "/signin";
+  }
+  
+  
+  
   return (
     <div className="nav" id={extraNavId}>
       <h3 className="nav-heading">Gorecco</h3>
@@ -26,25 +46,54 @@ const Navbar = ({ extraNavId }) => {
           }`}
         />
       </div>
-      <div>Messages</div>
+      {user?
+        <><div>Messages</div>
       <div>Bookings</div>
       <div>Favourites</div>
-      {user ? (
-        <>
-          <label>{user.personalInfo.fullName}</label> <Avatar />{" "}
-        </>
-      ) : (
-        <Link
+      <Button
+        aria-controls="simple-menu"
+        aria-haspopup="true"
+        onClick={Boolean(anchorEl)===false?handleClick:handleClose}
+      >
+        <Avatar/>
+      </Button>
+      <Menu
+        id="simple-menu"
+        anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+        getContentAnchorEl={null}
+        anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
+        transformOrigin={{ horizontal: "center" }}
+      >
+        <MenuItem >Hi,{user.personalInfo.fullName}</MenuItem>
+        <MenuItem onClick={handleClose}>Account Settings</MenuItem>
+        <MenuItem onClick={acntset}>Logout</MenuItem>
+      </Menu>
+      </>:
+      <>
+      <div>List your places</div>
+      <Link
           to={"/signin"}
           style={{
             color: "black",
-            fontSize: "16px",
-            textTransform: "capitalize",
+            fontSize: "19px"
           }}
         >
           Sign In
         </Link>
-      )}
+        <Link
+          to={"/signin"}
+          style={{
+            color: "black",
+            fontSize: "19px"
+          }}
+        >
+          Sign Up
+        </Link>
+      </>}
+      
     </div>
   );
 };
