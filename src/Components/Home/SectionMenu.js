e4e9b8b08669ed40432f98d6bcb5f08d42eeaa21
AryanshTripathi <aryansh.tripathi.civ20@itbhu.ac.in>
2022-10-05 20:33:35 +0530

    Checking

--- src/Components/Home/SectionMenu.js
+++ src/Components/Home/SectionMenu.js
@@ -26,6 +26,8 @@ import indi_img4 from "../../Assets/Images/indi-menu-4.jpeg";
 import indi_img5 from "../../Assets/Images/indi-menu-5.jpeg";
 import indi_img6 from "../../Assets/Images/indi-menu-6.jpeg";
 
+import arrow from "../../Assets/Images/border-arrow.jpeg";
+
 const SectionMenu = () => {
 	const filmShooting = [
 		{ image: film_img1, text: "Ad Film Shoot" },
@@ -69,6 +71,12 @@ const SectionMenu = () => {
 		"grid-menu-item-3",
 	];
 
+	const [arrow, setArrow] = useState({
+		film: true,
+		corporate: false,
+		individual: false,
+	});
+
 	let gridItems = shootMap[shoot].map((img, index) => (
 		<div className={classes[index]} key={index}>
 			<Link
@@ -93,11 +101,16 @@ const SectionMenu = () => {
 						borderBottom: `2px solid ${shoot === 1 ? "#ff6767" : "black"}`,
 						paddingBottom: "5px",
 						color: shoot === 1 ? "#ff6767" : "black",
+						position: "relative",
 					}}
 					onClick={() => setShoot(1)}>
 					<GiFilmProjector size="40px" />
 					<div className={`menu-heading-item`}>Film Shooting</div>
-					<AiOutlineDown size="15px" />
+					<img
+						src={arrow}
+						alt="Arrow"
+						className={`border-arrow ${arrow.film === false ? "hide" : ""}`}
+					/>
 				</div>
 				<div
 					style={{
@@ -108,11 +121,18 @@ const SectionMenu = () => {
 						borderBottom: `2px solid ${shoot === 2 ? "#ff6767" : "black"}`,
 						paddingBottom: "5px",
 						color: shoot === 2 ? "#ff6767" : "black",
+						position: "relative",
 					}}
 					onClick={() => setShoot(2)}>
 					<MdOutlineCorporateFare size="40px" />
 					<div className={`menu-heading-item`}>Corporate Shooting</div>
-					<AiOutlineDown size="15px" />
+					<img
+						src={arrow}
+						alt="Arrow"
+						className={`border-arrow ${
+							arrow.corporate === false ? "hide" : ""
+						}`}
+					/>
 				</div>
 				<div
 					style={{
@@ -123,11 +143,18 @@ const SectionMenu = () => {
 						borderBottom: `2px solid ${shoot === 3 ? "#ff6767" : "black"}`,
 						paddingBottom: "5px",
 						color: shoot === 3 ? "#ff6767" : "black",
+						position: "relative",
 					}}
 					onClick={() => setShoot(3)}>
 					<BsPersonFill size="40px" />
 					<div className={`menu-heading-item`}>Individual Shooting</div>
-					<AiOutlineDown size="15px" />
+					<img
+						src={arrow}
+						alt="Arrow"
+						className={`border-arrow ${
+							arrow.individual === false ? "hide" : ""
+						}`}
+					/>
 				</div>
 			</div>
 			<div className="grid-menu">{gridItems}</div>
