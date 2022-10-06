689cdd4388a7ccf9764427b4ce1c53dd743e6d37
SnehaSah2204 <snehasah2204@gmail.com>
2022-10-06 20:34:15 +0530

    frontend changes

--- src/Components/Home/Testimonials.js
+++ src/Components/Home/Testimonials.js
@@ -9,31 +9,31 @@ const Testimonials = () => {
 	const infoObj = [
 		{
 			image: img1,
-			name: "Full Name 1",
+			name: "Dhir Acharya",
 			testimony:
 				"When we decided to get married, we sought outdoor wedding venues to celebrate our special day. While there are many destination wedding places, finding the right one can be challenging. Luckily, we came across SpotLet and could instantly shortlist 3 locations for weddings. After speaking to the hosts and getting quotations, we found our dream wedding destination, and the entire process went smoothly! Thank you, SpotLet.",
 		},
 		{
 			image: img2,
-			name: "Full Name 2",
+			name: "Aadhya Patel",
 			testimony:
 				"After working from home for almost two years, our teams finally came to the office this year. However, the employees couldn’t bond well since we had many new recruits. So, we decided to host a corporate event, especially for the team and started looking for team meeting locations. Thankfully, SpotLet’s great list of venues helped us find the best corporate retreat centre at an affordable price. As a result, our teams are now indeed in sync. Thank you for this service.",
 		},
 		{
 			image: img3,
-			name: "Full Name 3",
+			name: "Divya Balakrishnan",
 			testimony:
 				"We planned to shoot for a feature film and needed a unique location with particular interiors. After searching a lot in Hyderabad, we still couldn’t find our dream shoot location. Then one of the crew members suggested searching on SpotLet. The wide range of film shooting locations helped us choose one immediately. The host was accommodating, and our shoot was a big success.",
 		},
 		{
 			image: img1,
-			name: "Full Name 4",
+			name: "Shrey Apte",
 			testimony:
 				"As a regional production house, we’re always looking for new and innovative spaces for our shoot locations. SpotLet allowed us to choose from hundreds of top video shoot locations within our locality, and we’re now their loyal customers! Every booking is easy, quick and affordable, and the hosts are always ready to help during the shoot. Thank you, SpotLet! We will definitely recommend their services.",
 		},
 		{
 			image: img2,
-			name: "Full Name 5",
+			name: "Manavi Malhotra",
 			testimony:
 				"Our company was planning a new product, and thus, we wanted a spacious area for the party. After searching for many product launch event locations, we still couldn’t find something that suited our budget and preferences. When we visited SpotLet, we found many sites we liked in just a few clicks! They genuinely have the best places for corporate events.",
 		},
