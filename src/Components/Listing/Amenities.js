83d3f37d90d04bfd54537e90eb75d4102e108964
AryanshTripathi <aryansh.tripathi.civ20@itbhu.ac.in>
2022-10-11 16:45:13 +0530

    Made some changees

--- src/Components/Listing/Amenities.js
+++ src/Components/Listing/Amenities.js
@@ -1,13 +1,16 @@
 import React, { useEffect, useState } from "react";
-import Select from 'react-select'
-import ClearIcon from '@mui/icons-material/Clear';
+import Select from "react-select";
+import ClearIcon from "@mui/icons-material/Clear";
 import { useDispatch, useSelector } from "react-redux";
-import { addLocation, selectLocationData, selectLocationId } from "../../redux/slices/locationSlice";
+import {
+	addLocation,
+	selectLocationData,
+	selectLocationId,
+} from "../../redux/slices/locationSlice";
 import { createTempLocation } from "../../services/api";
 import { toast } from "react-toastify";
 
-
-const Amenities = ({showSection}) => {
+const Amenities = ({ showSection }) => {
 	const options = [
 		{ value: "Air Conditioning", label: "Air Conditioning" },
 		{ value: "Wi-Fi", label: "Wi-Fi" },
@@ -15,14 +18,14 @@ const Amenities = ({showSection}) => {
 		{ value: "Projector", label: "Projector" },
 		{ value: "Paints", label: "Paints" },
 	];
-	const [amenities, setAmenities] = useState([]);
+	const [amenities, setAmenities] = useState(["Amenity 1", "Amenity 2"]);
 	const dispatch = useDispatch();
 	const location_id = useSelector(selectLocationId);
 	const location = useSelector(selectLocationData);
 
 	useEffect(() => {
 		location.amenities && setAmenities(location.amenities);
-	  }, [])
+	}, []);
 
 	const HandleChange = (e) => {
 		if (!amenities.includes(e.value)) {
@@ -31,37 +34,41 @@ const Amenities = ({showSection}) => {
 	};
 	const deleteoptn = (e) => {
 		amenities.splice(e, 1);
-		setAmenities((prev) => [...prev])
-	}
-	const handleSubmit = async(e) => {
+		setAmenities((prev) => [...prev]);
+	};
+	const handleSubmit = async (e) => {
 		e.preventDefault();
 		//console.log(amenities);
-		if(!amenities.length)
-		  return toast.error("Please fill all required fields!!!")
+		if (!amenities.length)
+			return toast.error("Please fill all required fields!!!");
 		const locData = {
 			...location,
-			amenities
-		  }
-		  dispatch(addLocation(locData));
-		  const form = {
+			amenities,
+		};
+		dispatch(addLocation(locData));
+		const form = {
 			location_id,
-			data : locData
-		  }
-		  try {
+			data: locData,
+		};
+		try {
 			await createTempLocation(form);
 			showSection("Photo");
-		  } catch (error) {
+		} catch (error) {
 			toast.error(error.response.data);
-		  }
-		
-
-	}
+		}
+	};
 	return (
 		<div className="lbox">
 			<div className="row1">
 				<div className="coll1">
-					<h2>Amenties<span style={{color : "red"}}>*</span></h2>
-					<Select className="select" options={options} onChange={HandleChange} />
+					<h2>
+						Amenties<span style={{ color: "red" }}>*</span>
+					</h2>
+					<Select
+						className="select"
+						options={options}
+						onChange={HandleChange}
+					/>
 				</div>
 			</div>
 			<div className="row1">
@@ -74,14 +81,15 @@ const Amenities = ({showSection}) => {
 								</div>
 								<ClearIcon onClick={() => deleteoptn(index)} />
 							</div>
-
 						</>
 					))}
 				</div>
 			</div>
-			<div className='row1'>
-				<div className='coll1'>
-					<button className='continue' onClick={handleSubmit}>Continue</button>
+			<div className="row1">
+				<div className="coll1">
+					<button className="continue" onClick={handleSubmit}>
+						Continue
+					</button>
 				</div>
 			</div>
 		</div>
