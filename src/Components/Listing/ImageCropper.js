a44dc3e2a443eced9bfc7e11582207bb0eecf926
SnehaSah2204 <snehasah2204@gmail.com>
2022-11-11 03:33:37 +0530

    Photos Working

---
+++ src/Components/Listing/ImageCropper.js
@@ -0,0 +1,134 @@
+
+import { Button } from "@mui/material";
+import React, { useState } from "react";
+import { useEffect } from "react";
+import ReactCrop, { centerCrop, makeAspectCrop } from "react-image-crop";
+import "react-image-crop/dist/ReactCrop.css";
+
+function ImageCropper(props) {
+  const {
+    imageToCrop,
+    onImageCropped,
+    onChangePreview,
+    croppedImage,
+    preview,
+  } = props;
+  const [cropConfig, setCropConfig] = useState(undefined);
+  const [imageRef, setImageRef] = useState();
+  const [adjustOpen, setAdjustOpen] = useState(true);
+
+  async function cropImage(crop) {
+    if (imageRef && crop?.width && crop?.height) {
+      const croppedImage = await getCroppedImage(
+        imageRef,
+        crop,
+        "croppedImage.jpeg" // destination filename
+      );
+
+      onImageCropped(croppedImage);
+    }
+  }
+
+  function getCroppedImage(sourceImage, cropConfig, fileName) {
+    // creating the cropped image from the source image
+    const canvas = document.createElement("canvas");
+    const scaleX = sourceImage.naturalWidth / sourceImage.width;
+    const scaleY = sourceImage.naturalHeight / sourceImage.height;
+    canvas.width = cropConfig.width;
+    canvas.height = cropConfig.height;
+    const ctx = canvas.getContext("2d");
+
+    ctx.drawImage(
+      sourceImage,
+      cropConfig.x * scaleX,
+      cropConfig.y * scaleY,
+      cropConfig.width * scaleX,
+      cropConfig.height * scaleY,
+      0,
+      0,
+      cropConfig.width,
+      cropConfig.height
+    );
+
+    return new Promise((resolve, reject) => {
+      canvas.toBlob((blob) => {
+        // returning an error
+        if (!blob) {
+          reject(new Error("Canvas is empty"));
+          return;
+        }
+
+        blob.name = fileName;
+        // creating a Object URL representing the Blob object given
+        const croppedImageUrl = window.URL.createObjectURL(blob);
+
+        resolve(croppedImageUrl);
+      }, "image/jpeg");
+    });
+  }
+
+  function onImageLoad(e) {
+    setImageRef(e.target);
+    const { naturalWidth: width, naturalHeight: height } = e.currentTarget;
+
+    const crop = centerCrop(
+      makeAspectCrop(
+        {
+          unit: "%",
+          width: 100,
+        },
+        16 / 9,
+        width,
+        height
+      ),
+      width,
+      height
+    );
+
+    setCropConfig(crop);
+  }
+  useEffect(() => {
+    cropImage();
+  }, [cropConfig]);
+
+  return (
+    <div>
+      <div
+        style={{ position: "relative", maxWidth: "400px", marginTop: "5px" }}
+      >
+        <ReactCrop
+          src={imageToCrop}
+          crop={cropConfig}
+          ruleOfThirds
+          onComplete={(cropConfig) => cropImage(cropConfig)}
+          onChange={(cropConfig, percentCrop) => setCropConfig(percentCrop)}
+          crossorigin="anonymous" // to avoid CORS-related problems
+          aspect={16 / 9}
+        >
+          {adjustOpen && <img src={imageToCrop} alt="" onLoad={onImageLoad} />}
+        </ReactCrop>
+        {imageRef && (
+          <div style={{ display: "flex", gap: "10px", marginBottom: "10px" }}>
+            <Button
+              variant="contained"
+              onClick={() => setAdjustOpen(!adjustOpen)} style={{background:"#f26767"}}
+            >
+              {adjustOpen ? "Close" : "Resize"}
+            </Button>
+            {croppedImage && (
+              <Button variant="contained" onClick={onChangePreview} style={{background:"#f26767"}}>
+                {preview ? "Minimize" : "Show Cropped"}
+              </Button>
+            )}
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
+
+ImageCropper.defaultProps = {
+  onImageCropped: () => {},
+};
+
+export default ImageCropper;
--- src/pages/ListingPlace.js
+++ src/pages/ListingPlace.js
@@ -1,3 +1,190 @@
+// import React, { useEffect, useState } from "react";
+// import "../Assets/Styles/ListingPlace.css";
+// import Details from "../Components/Listing/Details";
+// import Location from "../Components/Listing/Location";
+// import Amenities from "../Components/Listing/Amenities";
+// import Navbar from "../Components/Navbar";
+// import Photo from "../Components/Listing/Photo";
+// import Features from "../Components/Listing/Features";
+// import Dondont from "../Components/Listing/Dondont";
+// import Pricing from "../Components/Listing/Pricing";
+// import Rules from "../Components/Listing/Rules";
+// import Timings from "../Components/Listing/Timing";
+// import Contact from "../Components/Listing/Contact";
+// import BankDetails from "../Components/Listing/BankDetails";
+// import Gst from "../Components/Listing/Gst";
+// import TermCondition from "../Components/Listing/Term&Condition";
+// import ReviewApplication from "../Components/Listing/ReviewApplication";
+// import { useSelector } from "react-redux";
+// import { selectLocationData } from "../redux/slices/locationSlice";
+// import { AiOutlineArrowLeft } from 'react-icons/ai'
+
+// const ListingPlace = () => {
+
+// 	const [sec, setSec] = useState("Details & Description");
+
+// 	let x = window.matchMedia("(max-width: 576px)")
+// 	useEffect(() => {
+// 		if (x.matches)
+// 			setSec("");
+// 		  else
+// 			setSec("Details & Description");
+// 	}, [])
+
+// 	const [section, showSection] = useState(sec);
+// 	const data = useSelector(selectLocationData);
+// 	const handlesection = (e) => {
+// 		showSection(e);
+// 	};
+
+// 	const items = [
+// 		"Details & Description",
+// 		"Location",
+// 		"Amenities",
+// 		"Photos",
+// 		"Features",
+// 		"Do's & Don'ts",
+// 		"Pricing",
+// 		"Rules of the Host",
+// 		"Timings",
+// 		"Contact Details",
+// 		"GST Details",
+// 		"Bank Details",
+// 		"Review Application",
+// 		"Terms & Conditions",
+// 	];
+
+// 	const itemsObj = {
+// 		"Details & Description": 0,
+// 		"Location": 1,
+// 		"Amenities": 2,
+// 		"Photos": 3,
+// 		"Features": 4,
+// 		"Do's & Don'ts": 5,
+// 		"Pricing": 6,
+// 		"Rules of the Host": 7,
+// 		"Timings": 8,
+// 		"Contact Details": 9,
+// 		"GST Details": 10,
+// 		"Bank Details": 11,
+// 		"Review Application": 12,
+// 		"Terms & Conditions": 13,
+// 	};
+
+// 	const changeSection = (sect) => {
+// 		setSec(sect);
+// 		showSection(sect);
+// 	}
+// 	console.log(sec)
+// 	console.log(section)
+
+// 	return (
+// 		<>
+// 			<div>
+				
+// 				<Navbar extraNavId={"id-2"} />
+// 				<div className="host">
+
+// 					{!x.matches || !section.length ? <nav className="lnav-menu">
+// 						{items.map((item, ind) => (
+// 							<div key={ind}>
+// 								<button
+// 									onClick={() => itemsObj[item] <= itemsObj[section] ? handlesection(item) : ''}
+// 									onClick={() => handlesection(item)}
+// 									className={section === item ? "lnav-text sel" : "lnav-text"}
+// 									style={{color: itemsObj[item] > itemsObj[section] ? "grey" : ""}}>
+// 									{item}
+// 								</button>
+// 							</div>
+							
+// 						))}
+// 						</nav> : 
+// 						<div className="menu">
+// 							<AiOutlineArrowLeft size="30px" onClick={() => showSection("")} />
+// 							<h3>{section}</h3>
+// 						</div>
+// 						}
+					
+
+// 					<div
+// 						style={{
+// 							flexGrow: "1",
+// 						}}>
+// 						{section === "Details & Description" ? (
+// 							<Details showSection={handlesection} changeSection={changeSection} />
+// 						) : (
+// 							""
+// 						)}
+// 						{section === "Location" ? (
+// 							<Location showSection={handlesection} changeSection={changeSection} />
+// 						) : (
+// 							""
+// 						)}
+// 						{section === "Amenities" ? (
+// 							<Amenities showSection={handlesection} changeSection={changeSection} />
+// 						) : (
+// 							""
+// 						)}
+// 						{section === "Photos" ? <Photo showSection={handlesection} /> : ""}
+// 						{section === "Features" ? (
+// 							<Features showSection={handlesection} changeSection={changeSection} />
+// 						) : (
+// 							""
+// 						)}
+// 						{section === "Do's & Don'ts" ? (
+// 							<Dondont showSection={handlesection} changeSection={changeSection} />
+// 						) : (
+// 							""
+// 						)}
+// 						{section === "Pricing" ? (
+// 							<Pricing showSection={handlesection} changeSection={changeSection} />
+// 						) : (
+// 							""
+// 						)}
+// 						{section === "Rules of the Host" ? (
+// 							<Rules showSection={handlesection} changeSection={changeSection} />
+// 						) : (
+// 							""
+// 						)}
+// 						{section === "Timings" ? (
+// 							<Timings showSection={handlesection} changeSection={changeSection} />
+// 						) : (
+// 							""
+// 						)}
+// 						{section === "Contact Details" ? (
+// 							<Contact showSection={handlesection} changeSection={changeSection} />
+// 						) : (
+// 							""
+// 						)}
+// 						{section === "GST Details" ? (
+// 							<Gst showSection={handlesection} changeSection={changeSection} />
+// 						) : (
+// 							""
+// 						)}
+// 						{section === "Bank Details" ? (
+// 							<BankDetails showSection={handlesection} changeSection={changeSection} />
+// 						) : (
+// 							""
+// 						)}
+// 						{section === "Review Application" ? (
+// 							<ReviewApplication showSection={handlesection} changeSection={changeSection} />
+// 						) : (
+// 							""
+// 						)}
+// 						{section === "Terms & Conditions" ? (
+// 							<TermCondition showSection={handlesection} changeSection={changeSection} />
+// 						) : (
+// 							""
+// 						)}
+// 					</div>
+// 				</div>
+// 			</div>
+// 		</>
+// 	);
+// };
+
+// export default ListingPlace;
+
 import React, { useEffect, useState } from "react";
 import "../Assets/Styles/ListingPlace.css";
 import Details from "../Components/Listing/Details";
@@ -5,6 +192,7 @@ import Location from "../Components/Listing/Location";
 import Amenities from "../Components/Listing/Amenities";
 import Navbar from "../Components/Navbar";
 import Photo from "../Components/Listing/Photo";
+import Photos from "../Components/Listing/Photos";
 import Features from "../Components/Listing/Features";
 import Dondont from "../Components/Listing/Dondont";
 import Pricing from "../Components/Listing/Pricing";
@@ -17,170 +205,207 @@ import TermCondition from "../Components/Listing/Term&Condition";
 import ReviewApplication from "../Components/Listing/ReviewApplication";
 import { useSelector } from "react-redux";
 import { selectLocationData } from "../redux/slices/locationSlice";
-import { AiOutlineArrowLeft } from 'react-icons/ai'
+import { AiOutlineArrowLeft } from "react-icons/ai";
 
 const ListingPlace = () => {
+  const [sec, setSec] = useState("Details & Description");
 
-	const [sec, setSec] = useState("Details & Description");
+  let x = window.matchMedia("(max-width: 576px)");
+  useEffect(() => {
+    if (x.matches) setSec("");
+    else setSec("Details & Description");
+  }, []);
 
-	let x = window.matchMedia("(max-width: 576px)")
-	useEffect(() => {
-		if (x.matches)
-			setSec("");
-		  else
-			setSec("Details & Description");
-	}, [])
+  const [section, showSection] = useState(sec);
+  const data = useSelector(selectLocationData);
+  const handlesection = (e) => {
+    showSection(e);
+  };
 
-	const [section, showSection] = useState(sec);
-	const data = useSelector(selectLocationData);
-	const handlesection = (e) => {
-		showSection(e);
-	};
+  const items = [
+    "Details & Description",
+    "Location",
+    "Amenities",
+    "Photos",
+    "Features",
+    "Do's & Don'ts",
+    "Pricing",
+    "Rules of the Host",
+    "Timings",
+    "Contact Details",
+    "GST Details",
+    "Bank Details",
+    "Review Application",
+    "Terms & Conditions",
+  ];
 
-	const items = [
-		"Details & Description",
-		"Location",
-		"Amenities",
-		"Photos",
-		"Features",
-		"Do's & Don'ts",
-		"Pricing",
-		"Rules of the Host",
-		"Timings",
-		"Contact Details",
-		"GST Details",
-		"Bank Details",
-		"Review Application",
-		"Terms & Conditions",
-	];
+  const itemsObj = {
+    "Details & Description": 0,
+    Location: 1,
+    Amenities: 2,
+    Photos: 3,
+    Features: 4,
+    "Do's & Don'ts": 5,
+    Pricing: 6,
+    "Rules of the Host": 7,
+    Timings: 8,
+    "Contact Details": 9,
+    "GST Details": 10,
+    "Bank Details": 11,
+    "Review Application": 12,
+    "Terms & Conditions": 13,
+  };
 
-	const itemsObj = {
-		"Details & Description": 0,
-		"Location": 1,
-		"Amenities": 2,
-		"Photos": 3,
-		"Features": 4,
-		"Do's & Don'ts": 5,
-		"Pricing": 6,
-		"Rules of the Host": 7,
-		"Timings": 8,
-		"Contact Details": 9,
-		"GST Details": 10,
-		"Bank Details": 11,
-		"Review Application": 12,
-		"Terms & Conditions": 13,
-	};
+  const changeSection = (sect) => {
+    setSec(sect);
+    showSection(sect);
+  };
 
-	const changeSection = (sect) => {
-		setSec(sect);
-		showSection(sect);
-	}
-	console.log(sec)
-	console.log(section)
+  return (
+    <>
+      <div>
+        <Navbar extraNavId={"id-2"} />
+        <div className="host">
+          {!x.matches || !section.length ? (
+            <nav className="lnav-menu">
+              {items.map((item, ind) => (
+                <div key={ind}>
+                  <button
+                    // onClick={() =>
+                    //   itemsObj[item] <= itemsObj[section]
+                    //     ? handlesection(item)
+                    //     : ""
+                    // }
+                    onClick={() => handlesection(item)}
+                    className={section === item ? "lnav-text sel" : "lnav-text"}
+                    style={{
+                      color: itemsObj[item] > itemsObj[section] ? "grey" : "",
+                    }}
+                  >
+                    {item}
+                  </button>
+                </div>
+              ))}
+            </nav>
+          ) : (
+            <div className="menu">
+              <AiOutlineArrowLeft size="30px" onClick={() => showSection("")} />
+              <h3>{section}</h3>
+            </div>
+          )}
 
-	return (
-		<>
-			<div>
-				
-				<Navbar extraNavId={"id-2"} />
-				<div className="host">
-
-					{!x.matches || !section.length ? <nav className="lnav-menu">
-						{items.map((item, ind) => (
-							<div key={ind}>
-								<button
-									// onClick={() => itemsObj[item] <= itemsObj[section] ? handlesection(item) : ''}
-									onClick={() => handlesection(item)}
-									className={section === item ? "lnav-text sel" : "lnav-text"}
-									style={{color: itemsObj[item] > itemsObj[section] ? "grey" : ""}}>
-									{item}
-								</button>
-							</div>
-							
-						))}
-						</nav> : 
-						<div className="menu">
-							<AiOutlineArrowLeft size="30px" onClick={() => showSection("")} />
-							<h3>{section}</h3>
-						</div>
-						}
-					
-
-					<div
-						style={{
-							flexGrow: "1",
-						}}>
-						{section === "Details & Description" ? (
-							<Details showSection={handlesection} changeSection={changeSection} />
-						) : (
-							""
-						)}
-						{section === "Location" ? (
-							<Location showSection={handlesection} changeSection={changeSection} />
-						) : (
-							""
-						)}
-						{section === "Amenities" ? (
-							<Amenities showSection={handlesection} changeSection={changeSection} />
-						) : (
-							""
-						)}
-						{section === "Photos" ? <Photo showSection={handlesection} /> : ""}
-						{section === "Features" ? (
-							<Features showSection={handlesection} changeSection={changeSection} />
-						) : (
-							""
-						)}
-						{section === "Do's & Don'ts" ? (
-							<Dondont showSection={handlesection} changeSection={changeSection} />
-						) : (
-							""
-						)}
-						{section === "Pricing" ? (
-							<Pricing showSection={handlesection} changeSection={changeSection} />
-						) : (
-							""
-						)}
-						{section === "Rules of the Host" ? (
-							<Rules showSection={handlesection} changeSection={changeSection} />
-						) : (
-							""
-						)}
-						{section === "Timings" ? (
-							<Timings showSection={handlesection} changeSection={changeSection} />
-						) : (
-							""
-						)}
-						{section === "Contact Details" ? (
-							<Contact showSection={handlesection} changeSection={changeSection} />
-						) : (
-							""
-						)}
-						{section === "GST Details" ? (
-							<Gst showSection={handlesection} changeSection={changeSection} />
-						) : (
-							""
-						)}
-						{section === "Bank Details" ? (
-							<BankDetails showSection={handlesection} changeSection={changeSection} />
-						) : (
-							""
-						)}
-						{section === "Review Application" ? (
-							<ReviewApplication showSection={handlesection} changeSection={changeSection} />
-						) : (
-							""
-						)}
-						{section === "Terms & Conditions" ? (
-							<TermCondition showSection={handlesection} changeSection={changeSection} />
-						) : (
-							""
-						)}
-					</div>
-				</div>
-			</div>
-		</>
-	);
+          <div
+            style={{
+              flexGrow: "1",
+            }}
+          >
+            {section === "Details & Description" ? (
+              <Details
+                showSection={handlesection}
+                changeSection={changeSection}
+              />
+            ) : (
+              ""
+            )}
+            {section === "Location" ? (
+              <Location
+                showSection={handlesection}
+                changeSection={changeSection}
+              />
+            ) : (
+              ""
+            )}
+            {section === "Amenities" ? (
+              <Amenities
+                showSection={handlesection}
+                changeSection={changeSection}
+              />
+            ) : (
+              ""
+            )}
+            {section === "Photos" ? <Photos showSection={handlesection} /> : ""}
+            {section === "Features" ? (
+              <Features
+                showSection={handlesection}
+                changeSection={changeSection}
+              />
+            ) : (
+              ""
+            )}
+            {section === "Do's & Don'ts" ? (
+              <Dondont
+                showSection={handlesection}
+                changeSection={changeSection}
+              />
+            ) : (
+              ""
+            )}
+            {section === "Pricing" ? (
+              <Pricing
+                showSection={handlesection}
+                changeSection={changeSection}
+              />
+            ) : (
+              ""
+            )}
+            {section === "Rules of the Host" ? (
+              <Rules
+                showSection={handlesection}
+                changeSection={changeSection}
+              />
+            ) : (
+              ""
+            )}
+            {section === "Timings" ? (
+              <Timings
+                showSection={handlesection}
+                changeSection={changeSection}
+              />
+            ) : (
+              ""
+            )}
+            {section === "Contact Details" ? (
+              <Contact
+                showSection={handlesection}
+                changeSection={changeSection}
+              />
+            ) : (
+              ""
+            )}
+            {section === "GST Details" ? (
+              <Gst showSection={handlesection} changeSection={changeSection} />
+            ) : (
+              ""
+            )}
+            {section === "Bank Details" ? (
+              <BankDetails
+                showSection={handlesection}
+                changeSection={changeSection}
+              />
+            ) : (
+              ""
+            )}
+            {section === "Review Application" ? (
+              <ReviewApplication
+                showSection={handlesection}
+                changeSection={changeSection}
+              />
+            ) : (
+              ""
+            )}
+            {section === "Terms & Conditions" ? (
+              <TermCondition
+                showSection={handlesection}
+                changeSection={changeSection}
+              />
+            ) : (
+              ""
+            )}
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default ListingPlace;
