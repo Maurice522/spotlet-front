b611319deb77f767e206506dde6d8cbcb6651525
Shaad <ssidique610@gmail.com>
2022-11-20 12:09:42 +0530

    pricing changes

--- src/Components/Booking/SideSection.js
+++ src/Components/Booking/SideSection.js
@@ -11,218 +11,223 @@ import { useSelector } from "react-redux";
 import { selectUserData } from "../../redux/slices/userSlice";
 
 const style = {
-	position: "absolute",
-	top: "50%",
-	left: "50%",
-	display: "flex",
-	flexDirection: "column",
-	justifyContent: "center",
-	alignItems: "center",
-	transform: "translate(-50%, -50%)",
-	width: 400,
-	bgcolor: "background.paper",
-	border: "2px solid #000",
-	boxShadow: 24,
-	p: 4,
-	width: "70%"
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
+  alignItems: "center",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+  width: "70%",
 };
 
 const SideSection = ({
-	setReadyForRequest,
-	readyForRequest,
-	index,
-	setIndex,
-	userData,
-	ok,
-	setOk,
-	event,
-	v1,
-	v2,
-	v3,
-	v4,
-	v5,
-	v6,
-	tot_price,
+  setReadyForRequest,
+  readyForRequest,
+  index,
+  setIndex,
+  userData,
+  ok,
+  setOk,
+  event,
+  v1,
+  v2,
+  v3,
+  v4,
+  v5,
+  v6,
+  tot_price,
 }) => {
-	const [open, setOpen] = React.useState(false);
-	const handleOpen = () => setOpen(true);
-	const handleClose = () => setOpen(false);
-	const [locationData, setLocationData] = useState({});
-	const navigate = useNavigate();
-	const user_id = useSelector(selectUser_id);
-	const location_id = window.location.pathname.substring(1, 10);
-	const total_amt =
-		v3 === "12"
-			? v6 * v3 * 0.9 * 1.18 + 80
-			: v3 === "24"
-			? v6 * v3 * 0.8 * 1.18 + 80
-			: v6 * v3 * 1.18 + 80;
-	useEffect(() => {
-		getLocation(location_id)
-			.then((res) => setLocationData(res.data))
-			.catch((err) => console.log(err));
-	}, []);
-	//console.log(v1, v2, v3, v4, v5, v6, event, userData);
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+  const [locationData, setLocationData] = useState({});
+  const navigate = useNavigate();
+  const user_id = useSelector(selectUser_id);
+  const location_id = window.location.pathname.substring(1, 10);
+  const total_amt =
+    v3 === "12"
+      ? v6 * v3 * 0.9 * 1.18 + 80
+      : v3 === "24"
+      ? v6 * v3 * 0.8 * 1.18 + 80
+      : v6 * v3 * 1.18 + 80;
+  useEffect(() => {
+    getLocation(location_id)
+      .then((res) => setLocationData(res.data))
+      .catch((err) => console.log(err));
+  }, []);
+  //console.log(v1, v2, v3, v4, v5, v6, event, userData);
 
-	const day = v1.getDate()
-	const month = v1.getMonth() + 1;
-	const year = v1.getFullYear();
+  const day = v1?.getDate();
+  const month = v1?.getMonth() + 1;
+  const year = v1?.getFullYear();
 
-	const handleClick = async () => {
-		//console.log("clicked");
-		if (index === 0) {
-			setIndex(1);
-		} else if (index === 1) {
-			if (ok) {
-				setIndex(2);
-				setOk(false);
-			} else {
-				toast.error("Please accept the terms and conditions");
-			}
-		} else if (index === 2) {
-			if (
-				readyForRequest &&
-				userData.firstName !== "" &&
-				userData.lastName !== "" &&
-				userData.who_reserves !== "" &&
-				userData.dob !== "" &&
-				userData.message !== ""
-			) {
-				const bookingDet = {
-					activity: v5,
-					attendies: v4,
-					date: day + "-" + month + "-" + year,
-					duration_in_hours: v3,
-					event,
-					owner_id: locationData.property_desc.user_id,
-					property_id: location_id,
-					time: v2,
-					total_amt,
-					user_id,
-					user_data: {
-						fullName: userData.firstName + " " + userData.lastName,
-						who_reserves: userData.who_reserves,
-						[userData.who_reserves === "Individual" ? "profession" : "company"]:
-							userData.who_reserves === "Individual"
-								? userData.profession
-								: userData.company,
-						dob: userData.dob,
-						message: userData.message,
-					},
-				};
-				try {
-					await bookingRequest(bookingDet);
-					handleOpen();
-					setReadyForRequest(false);
-					setTimeout(() => {
-						window.location = "/bookinglist/:booking";
-					}, 3000);
-				} catch (error) {
-					console.log(error);
-				}
-			} else {
-				toast.error("Please fill all the fields");
-			}
-		}
-		window.scrollTo(0, 0)
-	};
+  const handleClick = async () => {
+    //console.log("clicked");
+    if (index === 0) {
+      setIndex(1);
+    } else if (index === 1) {
+      if (ok) {
+        setIndex(2);
+        setOk(false);
+      } else {
+        toast.error("Please accept the terms and conditions");
+      }
+    } else if (index === 2) {
+      if (
+        readyForRequest &&
+        userData.firstName !== "" &&
+        userData.lastName !== "" &&
+        userData.who_reserves !== "" &&
+        userData.dob !== "" &&
+        userData.message !== ""
+      ) {
+        const bookingDet = {
+          activity: v5,
+          attendies: v4,
+          date: day + "-" + month + "-" + year,
+          duration_in_hours: v3,
+          event,
+          owner_id: locationData.property_desc.user_id,
+          property_id: location_id,
+          time: v2,
+          total_amt: tot_price + 80,
+          user_id,
+          user_data: {
+            fullName: userData.firstName + " " + userData.lastName,
+            who_reserves: userData.who_reserves,
+            [userData.who_reserves === "Individual" ? "profession" : "company"]:
+              userData.who_reserves === "Individual"
+                ? userData.profession
+                : userData.company,
+            dob: userData.dob,
+            message: userData.message,
+          },
+        };
+        try {
+          await bookingRequest(bookingDet);
+          handleOpen();
+          setReadyForRequest(false);
+          setTimeout(() => {
+            window.location = "/bookinglist/:booking";
+          }, 3000);
+        } catch (error) {
+          console.log(error);
+        }
+      } else {
+        toast.error("Please fill all the fields");
+      }
+    }
+    window.scrollTo(0, 0);
+  };
 
-	tot_price = parseInt(tot_price);
-	let gst = v6 * v3 * 1.18;
-	gst = parseInt(gst);
-	console.log(v6, v3, gst)
-	// console.log(v1, v3, v4);
-	const userBooking = useSelector(selectUserData);
+  tot_price = parseInt(tot_price);
+  let gst = v6 * v3 * 1.18;
+  gst = parseInt(gst);
+  console.log(v6, v3, gst);
+  // console.log(v1, v3, v4);
+  const userBooking = useSelector(selectUserData);
 
-	// console.log(userBooking.portfolio[userBooking.portfolio.length - 1].bookingId)
+  // console.log(userBooking.portfolio[userBooking.portfolio.length - 1].bookingId)
 
-	return (
-		<div className="side-section">
-			<div className="side-section-image-wrapper">
-				<img
-					src={locationData?.imagesData?.at(0).image}
-					alt="booking-process"
-					className="image"
-				/>
-			</div>
+  return (
+    <div className="side-section">
+      <div className="side-section-image-wrapper">
+        <img
+          src={locationData?.imagesData?.at(0).image}
+          alt="booking-process"
+          className="image"
+        />
+      </div>
 
-			<div data-attribute-1>{location_id}</div>
-			<div data-attribute-2>Location</div>
-			<br/><br/>
+      <div data-attribute-1>{location_id}</div>
+      <div data-attribute-2>Location</div>
+      <br />
+      <br />
 
-			{/* <div className="booking-side-section-title">Reserved Date</div>
+      {/* <div className="booking-side-section-title">Reserved Date</div>
 			<div className="booking-side-section-info">
 				{format(new Date(Number(year), Number(month), Number(day)), "PPP")}
 			</div> */}
-			{/* <div className="booking-side-section-title">Reserved Time</div>
+      {/* <div className="booking-side-section-title">Reserved Time</div>
 			<div className="booking-side-section-info">{`${v3} Hours`}</div> */}
 
-			<div data-attribute-3>
-				<div data-attribute-4>
-				Total Price (including Gst)
-				</div>
-				<div data-attribute-4>₹ {parseInt(gst)}</div>
-			</div>
-			<div data-attribute-3>
-				<div data-attribute-4>Discounted Price</div>
-				<div data-attribute-4>₹ {tot_price}</div>
-			</div>
+      <div data-attribute-3>
+        <div data-attribute-4>Total Price (including Gst)</div>
+        <div data-attribute-4>₹ {parseInt(gst)}</div>
+      </div>
+      <div data-attribute-3>
+        <div data-attribute-4>Discounted Price</div>
+        <div data-attribute-4>₹ {tot_price}</div>
+      </div>
 
-			<div data-attribute-3>
-				<div data-attribute-4>Cleaning Fee (including Gst)</div>
-				<div data-attribute-4>₹ 40</div>
-			</div>
+      <div data-attribute-3>
+        <div data-attribute-4>Cleaning Fee (including Gst)</div>
+        <div data-attribute-4>₹ 40</div>
+      </div>
 
-			<div data-attribute-3>
-				<div data-attribute-4>Processing Fee (including Gst)</div>
-				<div data-attribute-4>₹ 40</div>
-			</div>
+      <div data-attribute-3>
+        <div data-attribute-4>Processing Fee (including Gst)</div>
+        <div data-attribute-4>₹ 40</div>
+      </div>
 
-			<div data-attribute-3>
-				<div data-attribute-1>Total</div>
-				<div data-attribute-1>₹ {tot_price+80}</div>
-			</div>
+      <div data-attribute-3>
+        <div data-attribute-1>Total</div>
+        <div data-attribute-1>₹ {tot_price + 80}</div>
+      </div>
 
-			<Button
-				variant="contained"
-				onClick={handleClick}
-				sx={{
-					width: "22vw",
-					backgroundColor: "#EA4235",
-					color: "white",
-					borderRadius: "4px",
-					marginTop: "10px",
-				}}>
-				{index === 2 ? "Request Availability" : "Continue"}
-			</Button>
+      <Button
+        variant="contained"
+        onClick={handleClick}
+        sx={{
+          width: "22vw",
+          backgroundColor: "#EA4235",
+          color: "white",
+          borderRadius: "4px",
+          marginTop: "10px",
+        }}
+      >
+        {index === 2 ? "Request Availability" : "Continue"}
+      </Button>
 
-			<Modal
-				aria-labelledby="transition-modal-title"
-				aria-describedby="transition-modal-description"
-				open={open}
-				onClose={handleClose}
-				closeAfterTransition
-				BackdropComponent={Backdrop}
-				BackdropProps={{
-					timeout: 500,
-				}}>
-				<Fade in={open}>
-					<Box sx={style}>
-						<MdDone size={50} color="green" />
-						<Typography id="transition-modal-title" variant="h6" component="h2">
-							Request Sent Successfully!
-						</Typography>
-						<Typography>
-							Booking Id: {userBooking?.portfolio.length !== 0 ? userBooking.portfolio[userBooking.portfolio.length - 1].bookingId : ""}
-						</Typography>
-						<Typography id="transition-modal-description" sx={{ mt: 2 }}>
-							You will be redirected to the bookings page in 3 seconds
-						</Typography>
-					</Box>
-				</Fade>
-			</Modal>
-		</div>
-	);
+      <Modal
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        open={open}
+        onClose={handleClose}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 500,
+        }}
+      >
+        <Fade in={open}>
+          <Box sx={style}>
+            <MdDone size={50} color="green" />
+            <Typography id="transition-modal-title" variant="h6" component="h2">
+              Request Sent Successfully!
+            </Typography>
+            <Typography>
+              Booking Id:{" "}
+              {userBooking?.portfolio.length !== 0
+                ? userBooking.portfolio[userBooking.portfolio.length - 1]
+                    .bookingId
+                : ""}
+            </Typography>
+            <Typography id="transition-modal-description" sx={{ mt: 2 }}>
+              You will be redirected to the bookings page in 3 seconds
+            </Typography>
+          </Box>
+        </Fade>
+      </Modal>
+    </div>
+  );
 };
 
 export default SideSection;
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -11,104 +11,109 @@ import { Link, useNavigate } from "react-router-dom";
 import Menu from "@mui/material/Menu";
 import MenuItem from "@mui/material/MenuItem";
 import { useEffect } from "react";
-import FiberManualRecordIcon from '@mui/icons-material/FiberManualRecord'; import { userUpdate } from "../services/api";
+import FiberManualRecordIcon from "@mui/icons-material/FiberManualRecord";
+import { userUpdate } from "../services/api";
 
 const Navbar = ({ extraNavId }) => {
+  const user = useSelector(selectUserData);
+  const user_id = useSelector(selectUser_id);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const logout = () => {
+    handleClose();
+    localStorage.clear();
+    window.location = "/";
+  };
 
-	const user = useSelector(selectUserData);
-	const user_id = useSelector(selectUser_id);
-	const dispatch = useDispatch();
-	const navigate = useNavigate();
-	const logout = () => {
-		handleClose();
-		localStorage.clear();
-		window.location = "/";
-	};
+  const [flag, setFlag] = useState();
 
-	const [flag, setFlag] = useState()
+  const [anchorEl, setAnchorEl] = useState(null);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
-	const [anchorEl, setAnchorEl] = useState(null);
-	const handleClick = (event) => {
-		setAnchorEl(event.currentTarget);
-	};
-	const handleClose = () => {
-		setAnchorEl(null);
-	};
+  const handleNotification = async () => {
+    const updatedUserData = {
+      ...user,
+      notificationFlag: false,
+    };
+    const data = {
+      updatedUserData,
+      user_id,
+    };
+    console.log(data);
+    try {
+      await userUpdate(data);
+      window.location.reload(true);
+      window.location = "/notifications";
+    } catch (error) {
+      toast.error(error.response);
+    }
+  };
 
-	const handleNotification = async () => {
-		const updatedUserData = {
-			...user,
-			notificationFlag: false,
-		}
-		const data = {
-			updatedUserData,
-			user_id
-		}
-		console.log(data);
-		try {
-			await userUpdate(data);
-			window.location.reload(true);
-			window.location = "/notifications";
-		} catch (error) {
-			toast.error(error.response);
-		}
-	};
+  const [notifications, setNotifications] = useState([]);
+  useEffect(() => {
+    console.log(user);
+    setNotifications(user?.notifications);
+    setFlag(user?.notificationFlag);
+    // console.log(notifications);
+    console.log(flag);
+  }, [user]);
 
-	const [notifications, setNotifications] = useState([])
-	useEffect(() => {
-		console.log(user);
-		setNotifications(user?.notifications);
-		setFlag(user?.notificationFlag)
-		// console.log(notifications);
-		console.log(flag);
-	}, [user])
-
-	// const [NotificationEl, setNotificationEl] = useState(null);
-	// const clickHandler = (event) => {
-	// 	setNotificationEl(event.currentTarget);
-	// };
-	// const closeHandler = () => {
-	// 	setNotificationEl(null);
-	// };
-	const acntset = () => {
-		handleClose();
-		console.log("clicked");
-		navigate("/account");
-	};
-	let firstName = user?.personalInfo?.fullName.split(" ").slice(0, -1).join(" ");
-	let profilePic = user?.personalInfo?.profile_pic;
-	return (
-		<div className="nav" id={extraNavId}>
-			<Link to="/" onClick={() => window.scrollTo(0, 0)}>
-				<div className="nav-logo">
-					<img src={require(`../Assets/Images/logo.png`)} alt="logo" />
-				</div>
-			</Link>
-			<div className={`nav-search ${extraNavId !== "" ? "blacken" : ""}`}>
-				<AiOutlineSearch size="20px" />
-				{/* <TextField
+  // const [NotificationEl, setNotificationEl] = useState(null);
+  // const clickHandler = (event) => {
+  // 	setNotificationEl(event.currentTarget);
+  // };
+  // const closeHandler = () => {
+  // 	setNotificationEl(null);
+  // };
+  const acntset = () => {
+    handleClose();
+    console.log("clicked");
+    navigate("/account");
+  };
+  let firstName = user?.personalInfo?.fullName
+    .split(" ")
+    .slice(0, -1)
+    .join(" ");
+  let profilePic = user?.personalInfo?.profile_pic;
+  return (
+    <div className="nav" id={extraNavId}>
+      <Link to="/" onClick={() => window.scrollTo(0, 0)}>
+        <div className="nav-logo">
+          <img src={require(`../Assets/Images/logo.png`)} alt="logo" />
+        </div>
+      </Link>
+      <div className={`nav-search ${extraNavId !== "" ? "blacken" : ""}`}>
+        <AiOutlineSearch size="20px" />
+        {/* <TextField
 					label="Search"
 					variant="standard"
 					size="small"
 					// className="nav-search-input"
 				/> */}
-				<input
-					type="text"
-					placeholder="Search"
-					className={`nav-search-input ${extraNavId !== "" ? "blacken" : "whiten"
-						}`}
-				/>
-			</div>
-			<div className="nav-items">
-				{user ? (
-					<div
-						style={{
-							display: "flex",
-							jusitfyContent: "space-around",
-							alignItems: "center",
-							gap: "20px",
-						}}>
-						{/* <div onClick={Boolean(NotificationEl) === false ? clickHandler : closeHandler} style={{cursor: "pointer"}}>
+        <input
+          type="text"
+          placeholder="Search"
+          className={`nav-search-input ${
+            extraNavId !== "" ? "blacken" : "whiten"
+          }`}
+        />
+      </div>
+      <div className="nav-items">
+        {user ? (
+          <div
+            style={{
+              display: "flex",
+              jusitfyContent: "space-around",
+              alignItems: "center",
+              gap: "20px",
+            }}
+          >
+            {/* <div onClick={Boolean(NotificationEl) === false ? clickHandler : closeHandler} style={{cursor: "pointer"}}>
 							<div>Notifications</div>
 						</div>
 						<Menu
@@ -146,125 +151,151 @@ const Navbar = ({ extraNavId }) => {
 								)
 							}
 						</Menu> */}
-						{console.log(flag)}
-						<a>
-							<div onClick={handleNotification} style={{ cursor: "pointer" }}>
-								Notifications{flag && <FiberManualRecordIcon styles={{ color: "red" }} />}
-							</div>
-						</a>
-						<Link to="/messages" onClick={() => window.scrollTo(0, 0)}>
-							<div>Messages</div>
-						</Link>
-						<Link to="/bookinglist/:booking" onClick={() => window.scrollTo(0, 0)}>
-							<div>Bookings</div>
-						</Link>
-						<Link to="/favorite" onClick={() => window.scrollTo(0, 0)}>
-							<div>Favorites</div>
-						</Link>
-						<Link to="/listing" onClick={() => window.scrollTo(0, 0)}>
-							<div>List Your Space</div>
-						</Link>
-						<Button
-							aria-controls="simple-menu"
-							aria-haspopup="true"
-							onClick={Boolean(anchorEl) === false ? handleClick : handleClose}>
-							{profilePic ? (
-								<img className="user-dp" src={profilePic} alt="profile" />
-							) : (
-								<Avatar />
-							)}
-						</Button>
-						<Menu
-							id="simple-menu"
-							anchorEl={anchorEl}
-							keepMounted
-							open={Boolean(anchorEl)}
-							onClose={handleClose}
-							getContentAnchorEl={null}
-							anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
-							transformOrigin={{ horizontal: "center" }}>
-							<MenuItem>Hi, {firstName}</MenuItem>
-							<MenuItem onClick={acntset}>Account Settings</MenuItem>
-							<MenuItem onClick={logout}>Logout</MenuItem>
-						</Menu>
-					</div>
-				) : (
-					<div
-						style={{
-							display: "flex",
-							jusitfyContent: "space-around",
-							alignItems: "center",
-							gap: "20px",
-						}}>
-						<div onClick={() => {
-							toast.error("Please sign in first")
-							navigate("/signin");
-						}
-						}>
-							List your Space
-						</div>
-						<Link to={"/signin"} state={{ isSignIn: true }}>
-							Sign In
-						</Link>
-						<Link to={"/signin"} state={{ isSignIn: false }}>
-							Sign Up
-						</Link>
-					</div>
-				)}
-			</div>
-			<div className="nav-items-resp">
-				{user ? (
-					<div>
-						<Link to="/listing">
-							<AiOutlineSearch size="15px" color="#ff6767" />
-							<div>List Space</div>
-						</Link>
-						<Link to="/bookinglist/:booking">
-							<BiBookmark size="15px" color="#ff6767" />
-							<div>Bookings</div>
-						</Link>
-						<Link to="/">
-							<AiOutlineHome size="15px" color="#ff6767" />
-							<div>Home</div>
-						</Link>
-						<Link to="/favorite">
-							<AiOutlineHeart size="15px" color="#ff6767" />
-							<div>Favorites</div>
-						</Link>
-						<button onClick={Boolean(anchorEl) === false ? handleClick : handleClose}>
-							<CgProfile size="15px" color="#ff6767" />
-							<div>Account</div>
-						</button>
-						<Menu
-							id="simple-menu"
-							anchorEl={anchorEl}
-							keepMounted
-							open={Boolean(anchorEl)}
-							onClose={handleClose}
-							getContentAnchorEl={null}
-							anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
-							transformOrigin={{ horizontal: "center" }}>
-							<MenuItem>Hi, {firstName}</MenuItem>
-							<MenuItem onClick={acntset}>Account Settings</MenuItem>
-							<MenuItem onClick={logout}>Logout</MenuItem>
-						</Menu>
-					</div>
-				) : (
-					<div>
-						<div onClick={() => toast.error("Please sign in first")}>
-							List Space
-						</div>
-						<Link to={"/signin"} state={{ isSignIn: true }}>
-							Sign In
-						</Link>
-						<Link to={"/signin"} state={{ isSignIn: false }}>
-							Sign Up
-						</Link>
-					</div>
-				)}
-			</div>
-		</div>
-	);
+            {console.log(flag)}
+            <a>
+              <div
+                onClick={handleNotification}
+                style={{ cursor: "pointer", position: "relative" }}
+              >
+                Notifications
+                {flag && (
+                  <div
+                    style={{
+                      height: "12px",
+                      width: "12px",
+                      backgroundColor: "orange",
+                      borderRadius: "100%",
+                      postion: "absolute",
+                      top: "-10px",
+                      right: "-5px",
+                    }}
+                  />
+                )}
+              </div>
+            </a>
+            <Link to="/messages" onClick={() => window.scrollTo(0, 0)}>
+              <div>Messages</div>
+            </Link>
+            <Link
+              to="/bookinglist/:booking"
+              onClick={() => window.scrollTo(0, 0)}
+            >
+              <div>Bookings</div>
+            </Link>
+            <Link to="/favorite" onClick={() => window.scrollTo(0, 0)}>
+              <div>Favorites</div>
+            </Link>
+            <Link to="/listing" onClick={() => window.scrollTo(0, 0)}>
+              <div>List Your Space</div>
+            </Link>
+            <Button
+              aria-controls="simple-menu"
+              aria-haspopup="true"
+              onClick={Boolean(anchorEl) === false ? handleClick : handleClose}
+            >
+              {profilePic ? (
+                <img className="user-dp" src={profilePic} alt="profile" />
+              ) : (
+                <Avatar />
+              )}
+            </Button>
+            <Menu
+              id="simple-menu"
+              anchorEl={anchorEl}
+              keepMounted
+              open={Boolean(anchorEl)}
+              onClose={handleClose}
+              getContentAnchorEl={null}
+              anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
+              transformOrigin={{ horizontal: "center" }}
+            >
+              <MenuItem>Hi, {firstName}</MenuItem>
+              <MenuItem onClick={acntset}>Account Settings</MenuItem>
+              <MenuItem onClick={logout}>Logout</MenuItem>
+            </Menu>
+          </div>
+        ) : (
+          <div
+            style={{
+              display: "flex",
+              jusitfyContent: "space-around",
+              alignItems: "center",
+              gap: "20px",
+            }}
+          >
+            <div
+              onClick={() => {
+                toast.error("Please sign in first");
+                navigate("/signin");
+              }}
+            >
+              List your Space
+            </div>
+            <Link to={"/signin"} state={{ isSignIn: true }}>
+              Sign In
+            </Link>
+            <Link to={"/signin"} state={{ isSignIn: false }}>
+              Sign Up
+            </Link>
+          </div>
+        )}
+      </div>
+      <div className="nav-items-resp">
+        {user ? (
+          <div>
+            <Link to="/listing">
+              <AiOutlineSearch size="15px" color="#ff6767" />
+              <div>List Space</div>
+            </Link>
+            <Link to="/bookinglist/:booking">
+              <BiBookmark size="15px" color="#ff6767" />
+              <div>Bookings</div>
+            </Link>
+            <Link to="/">
+              <AiOutlineHome size="15px" color="#ff6767" />
+              <div>Home</div>
+            </Link>
+            <Link to="/favorite">
+              <AiOutlineHeart size="15px" color="#ff6767" />
+              <div>Favorites</div>
+            </Link>
+            <button
+              onClick={Boolean(anchorEl) === false ? handleClick : handleClose}
+            >
+              <CgProfile size="15px" color="#ff6767" />
+              <div>Account</div>
+            </button>
+            <Menu
+              id="simple-menu"
+              anchorEl={anchorEl}
+              keepMounted
+              open={Boolean(anchorEl)}
+              onClose={handleClose}
+              getContentAnchorEl={null}
+              anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
+              transformOrigin={{ horizontal: "center" }}
+            >
+              <MenuItem>Hi, {firstName}</MenuItem>
+              <MenuItem onClick={acntset}>Account Settings</MenuItem>
+              <MenuItem onClick={logout}>Logout</MenuItem>
+            </Menu>
+          </div>
+        ) : (
+          <div>
+            <div onClick={() => toast.error("Please sign in first")}>
+              List Space
+            </div>
+            <Link to={"/signin"} state={{ isSignIn: true }}>
+              Sign In
+            </Link>
+            <Link to={"/signin"} state={{ isSignIn: false }}>
+              Sign Up
+            </Link>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
